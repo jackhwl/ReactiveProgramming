d52d6c8f526e4d71c1e2a01aeb1f88149303c4cb
Wenlin Huang <jackhwl@hotmail.com>
2020-11-14 10:35:27 -0500

    Using subscription lambdas

---
+++ RxNetMath/Observer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RxNetMath
+{
+	class Observer : IObserver<int>
+	{
+		public void OnNext(int value)
+		{
+			var threadId = Thread.CurrentThread.ManagedThreadId.ToString();
+			Console.WriteLine($"Value received on thread {threadId}: {value}");
+		}
+
+		public void OnError(Exception error)
+		{
+			var threadId = Thread.CurrentThread.ManagedThreadId.ToString();
+			Console.WriteLine($"Error reported on thread {threadId}: {error.Message}");
+		}
+
+		public void OnCompleted()
+		{
+			var threadId = Thread.CurrentThread.ManagedThreadId.ToString();
+			Console.WriteLine($"Observation completed on thread {threadId}.");
+		}
+	}
+}
---
+++ RxNetMath/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace RxNetMath
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// https://www.dofactory.com/net/observer-design-pattern
+			// https://www.youtube.com/watch?v=kgpUrk1t5xM&list=PLHfwoPeLRqw7XOffuusep6CJRo6Yh6IYq&index=3
+			// https://sachabarbs.wordpress.com/2016/12/23/rx-over-the-wire/
+
+			var threadId = Thread.CurrentThread.ManagedThreadId.ToString();
+			Console.WriteLine($"Program started on main thread with Id {threadId}...");
+
+			var observable = Observable.Range(5,8);
+			var subscription = observable.Subscribe(new Observer());
+
+			Console.ReadKey();
+
+			subscription.Dispose();
+		}
+	}
+}
