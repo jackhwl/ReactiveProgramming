0ad7283d001e40c16f11d89212e9d63adafaa738
Wenlin Huang <jackhwl@hotmail.com>
2020-11-15 20:22:14 -0500

    Obserable

--- RxNetMath/Program.cs
+++ RxNetMath/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading;
 
 namespace RxNetMath
@@ -13,8 +14,16 @@ namespace RxNetMath
 			// https://www.youtube.com/watch?v=kgpUrk1t5xM&list=PLHfwoPeLRqw7XOffuusep6CJRo6Yh6IYq&index=3
 			// https://sachabarbs.wordpress.com/2016/12/23/rx-over-the-wire/
 
+			var mgr = new ReactiveSocialNetworksManager();
+			IDisposable subscription = mgr.ObserveMessages("Rx")
+				.Subscribe(
+				msg => Console.WriteLine($"Observed: {msg} \t"),
+				ex => { /*OnError*/},
+				() => {  /*OnCompleted*/});
 			//Demo();
-			ObserverBaseMitigatesContractViolation1();
+			//ObserverBaseMitigatesContractViolation1();
+			//SubjectMethod();
+
 
 			//var threadId = Thread.CurrentThread.ManagedThreadId.ToString();
 			//Console.WriteLine($"Program started on main thread with Id {threadId}...");
@@ -28,7 +37,7 @@ namespace RxNetMath
 			//);
 			//var subscription = observable.Subscribe(observer);
 
-			//Console.ReadKey();
+			Console.ReadKey();
 
 			//subscription.Dispose();
 		}
@@ -74,5 +83,16 @@ namespace RxNetMath
 			subscription.Dispose();
 		}
 
+		static void SubjectMethod()
+		{
+			var subject = new Subject<int>();
+			var subscription = subject.Subscribe(Console.WriteLine);
+
+			subject.OnNext(1);
+			subject.OnNext(2);
+
+			subscription.Dispose();
+			subject.OnNext(3);
+		}
 	}
 }
---
+++ RxNetMath/ReactiveSocialNetworksManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace RxNetMath
+{
+	class ReactiveSocialNetworksManager
+	{
+		public IObservable<Message> ObserveMessages(string hashtag)
+		{
+			return Observable.Create<Message>(async (observer) =>
+			{
+				var message = await GetMessagesAsync(hashtag);
+				foreach (var m in ObserveMessages)
+				{
+					observer.OnNext(m);
+				}
+				observer.OnCompleted();
+			});
+
+			//var messages = await GetMessagesAsync(hashtag);
+			//return messages.ToObservable();
+		}
+	}
+
+	public class Message
+	{
+	}
+}
