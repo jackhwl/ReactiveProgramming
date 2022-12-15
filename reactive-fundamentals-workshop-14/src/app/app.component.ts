0cd7a5d051c19cacd75dd1465c8c7aca2bf7bc09
Wenlin Huang <whuang@viglobal.com>
2022-12-14 22:18:14 -0500

    get prople marble test

--- reactive-fundamentals-workshop-14/src/app/app.component.ts
+++ reactive-fundamentals-workshop-14/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reactive-fundamentals-workshop-14';
+  title = 'marbles';
 }
---
+++ reactive-fundamentals-workshop-14/src/app/data.service.spec.ts
@@ -0,0 +1,31 @@
+import { DataService } from './data.service';
+import { Person } from './sw.models';
+import { cold } from 'jasmine-marbles';
+import { TestScheduler } from 'rxjs/testing';
+import { throttleTime } from 'rxjs/operators';
+
+fdescribe('DataService', () => {
+  let mockPeople:any;
+  beforeEach(() => {
+    mockPeople = [
+      { name: 'Juke Piewalker' },
+      { name: 'Sobe Han' }
+    ] as Person[];
+  });
+  describe('getPeople', () => {
+    let mockHttp:any;
+    let dataService: DataService;
+    beforeEach(() => {
+      mockHttp = jasmine.createSpyObj('HttpClient', ['get']);
+      dataService = new DataService(mockHttp);
+    });
+
+    it('should get people', () => {
+      const httpCall = cold('x|', { x: {results: mockPeople }})
+      mockHttp.get.and.returnValue(httpCall)
+      const actual = dataService.getPeople()
+      const expected = cold('a|', { a: mockPeople })
+      expect(actual).toBeObservable(expected)
+    })
+  });
+});
